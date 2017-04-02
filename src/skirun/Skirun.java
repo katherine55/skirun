fe74c05e57e1a3ecbf521334a91827269fef02b6
katherine55 <league@WorthyMini-3.attlocal.net>
2017-04-02 11:22:43 -0700

    changes

---
+++ src/skirun/Skirun.java
@@ -0,0 +1,25 @@
+package skirun;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Skirun {
+	
+	
+	Skirun(){
+		
+		myFrame.setVisible(true);
+		
+	}
+
+	JFrame myFrame = new JFrame();
+	static int width;
+	static int height;
+	
+	public static void main(String[] args) {
+		
+		new Skirun();
+
+	}
+
+}
